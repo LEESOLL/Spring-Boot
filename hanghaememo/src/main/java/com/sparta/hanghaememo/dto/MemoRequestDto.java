245e0c10218e4ecb533aeb5fbf571ee85649deaa
Lee Sol <leeso4567@naver.com>
2022-12-07 14:25:07 +0900

    memo프로그램 만들기

---
+++ hanghaememo/src/main/java/com/sparta/hanghaememo/dto/MemoRequestDto.java
@@ -0,0 +1,8 @@
+package com.sparta.hanghaememo.dto;
+
+import lombok.Getter;
+@Getter
+public class MemoRequestDto {
+    private String username;
+    private String contents;
+}
---
+++ hanghaememo/src/main/java/com/sparta/hanghaememo/entity/Memo.java
@@ -0,0 +1,37 @@
+package com.sparta.hanghaememo.entity;
+
+import com.sparta.hanghaememo.dto.MemoRequestDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Memo extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String contents;
+
+    public Memo(String username, String contents) {
+        this.username = username;
+        this.contents = contents;
+    }
+
+    public Memo(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+
+    public void update(MemoRequestDto requestDto) {
+        this.username = requestDto.getUsername();
+        this.contents = requestDto.getContents();
+    }
+
+}
